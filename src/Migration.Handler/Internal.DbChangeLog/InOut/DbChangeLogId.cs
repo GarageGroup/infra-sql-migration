b029f41f0a8d0a4fe099f05a62c5b252657e453e
pmosk <pm@ggroupp.ru>
2023-02-27 14:19:51 +0400

    Remove code generator

--- src/Migration.Handler/Internal.DbChangeLog/InOut/DbChangeLogId.cs
+++ src/Migration.Handler/Internal.DbChangeLog/InOut/DbChangeLogId.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GGroupp.Infra;
 
-[DbEntity]
 internal sealed partial record class DbChangeLogId : IDbEntity<DbChangeLogId>
 {
+    public static DbChangeLogId ReadEntity(IDbItem dbItem)
+    {
+        ArgumentNullException.ThrowIfNull(dbItem);
+
+        return new()
+        {
+            Id = dbItem.GetFieldValueOrThrow("Id")
+        };
+    }
+
     private readonly string? id;
 
-    [DbField]
+    [AllowNull]
     public string Id
     {
         get => id.OrEmpty();
